baa69980a707276a8124971d16fd0f9484b81462
Christopher Fennessey <christopherfennessey@Christophers-MacBook-Pro.local>
2018-08-14 11:19:06 -0400

    Added DB Config

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ var bodyParser = require("body-parser");
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
-
 // BodyParser makes it possible for our server to interpret data sent to it.
 // The code below is pretty standard.
 app.use(bodyParser.json());
@@ -38,8 +35,8 @@ require("./routes/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // ==============================================================================
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 
