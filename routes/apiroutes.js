c8deeac0ecb285381ab3b12893d1263f0467308e
Christopher Fennessey <christopherfennessey@Christophers-MacBook-Pro.local>
2018-08-14 10:59:48 -0400

    Added Search Bar

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -23,7 +23,7 @@ module.exports = function(app) {
   });
 
   app.get('/supplierdata', function (req, res) {
-    connection.query('select supplier from customers', function (error, results, fields) {
+    connection.query('SELECT * FROM customers WHERE invoiceDate BETWEEN >= 2017-09-15 and 2017-09-30', function (error, results, fields) {
       if (error) throw error;
       res.json(results);
       console.log(results[1].supplier);
